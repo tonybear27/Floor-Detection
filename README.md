12fd1c4687f6086af7c8ce64f7cacb3bc28fffb5
tonybear27 <tony20011027@gmail.com>
2024-08-23 14:16:37 +0800

    08.23.2024

---
+++ README.md
@@ -0,0 +1,90 @@
+## Environment Set Up
+
+### Create the environment, enter the directory and install some needed packages
+
+```bash
+conda create -n AMR python=3.8.5 && conda activate AMR`
+cd object-detection-api
+
+make install
+```
+
+if encounter this error 
+```bash
+option --use-feature: invalid choice: '2020-resolver' (choose from 'fast-deps', 'truststore', 'no-binary-enable-wheel-cache')
+make: *** [Makefile:12: api] Error 2
+```
+then open the Makefile, change line 15 from 
+
+`python -m pip install --use-feature=2020-resolver .`
+
+to 
+
+`python -m pip install .` 
+
+and try again
+
+
+if it installs successfully, then it would show
+
+```bash
+....................
+----------------------------------------------------------------------
+Ran 24 tests in 8.664s
+
+OK (skipped=1)
+```
+
+Install other packages
+
+`pip install -r requirements.txt`
+
+## Running
+
+Put your testing images at [here](object-detection-api/workspace/test-mask/images/test)
+
+And the result will be stored at [here](object-detection-api/workspace/test-mask/images/test_annotated)
+
+`python test_images.py` or if you prefer using [Jupter Notebook](test_image.ipynb)
+
+## Training
+
+Put your training images at [here](object-detection-api/workspace/test-mask/images/train), and your validation images at [here](object-detection-api/workspace/test-mask/images/val)
+
+**Note:** Your annotation files must follow COCO format.
+
+`make gen-tfrecord` to generate `label_map.pbtxt`
+
+### Download the Pre-trained Model
+
+`make dl-model` (The default selection here is **mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8**)
+
+And the structure will be
+
+```bash
+└── test-mask
+    └── pre-trained-models
+        └── mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8
+            ├── checkpoint
+            ├── pipeline.config
+            └── saved_model
+```
+
+### Configure Training Pipeline
+
+Create a corresponding model folder in the models directory, such as: **mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8**, and copy \ 
+**pre-trained-models/mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8/pipeline.config**
+
+```bash
+└── test-mask
+    ├── models
+    │   └── mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8
+    │       └── pipeline.config
+    └── pre-trained-models
+```
+
+`make train`
+
+## Model Export
+
+`make export`
---
+++ test_images.py
@@ -0,0 +1,158 @@
+import glob
+import os
+import time
+import tensorflow as tf
+import numpy as np
+import psutil    
+import GPUtil
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+
+from object_detection.utils import label_map_util
+from object_detection.utils import ops as utils_ops
+from PIL import Image
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+import warnings
+
+# Suppress the specific FutureWarning
+warnings.filterwarnings("ignore", category=FutureWarning, 
+                        message="You are using `torch.load` with `weights_only=False`")
+
+IMAGE_DIR = './images/test/'
+IMAGE_PATHS = glob.glob(os.path.join(IMAGE_DIR, '*'))
+
+# Load model
+PATH_TO_SAVED_MODEL = './exported-models/mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8' + "/saved_model"
+SAM_CHECKPOINT = 'sam_vit_h_4b8939.pth'
+MODEL_TYPE = 'vit_h'
+DEVICE = 'cpu' if torch.cuda.is_available() else 'cpu'
+
+# Load TensorFlow model
+print('Loading TensorFlow model...', end='')
+start_time = time.time()
+detect_fn = tf.saved_model.load(PATH_TO_SAVED_MODEL)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f'Done! Took {elapsed_time:.2f} seconds')
+
+# Free up memory if needed
+torch.cuda.empty_cache()
+
+# Load SAM model
+print('Loading SAM model...', end='')
+start_time = time.time()
+sam = sam_model_registry[MODEL_TYPE](checkpoint=SAM_CHECKPOINT)
+sam.to(DEVICE)
+predictor = SamPredictor(sam)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f'Done! Took {elapsed_time:.2f} seconds')
+
+# Labels
+PATH_TO_LABELS = './annotations/label_map.pbtxt'
+category_index = label_map_util.create_category_index_from_labelmap(
+    PATH_TO_LABELS, use_display_name=True)
+
+def log_hardware_usage():
+
+    ram_usage = psutil.virtual_memory()
+    cpu_usage = f"CPU RAM Free: {ram_usage.free / (1024 ** 3):.2f}GB | Used: {ram_usage.used / (1024 ** 3):.2f}GB | Util {ram_usage.percent:3.0f}% | Total {ram_usage.total / (1024 ** 3):.2f}GB"
+    
+    gpu = GPUtil.getGPUs()[0]
+    gpu_usage = f"GPU RAM Free: {gpu.memoryFree / 1024:.2f}GB | Used: {gpu.memoryUsed / 1024:.2f}GB | Util {gpu.memoryUtil * 100:3.0f}% | Total {gpu.memoryTotal / 1024:.2f}GB"
+    
+    return cpu_usage, gpu_usage
+
+
+def load_image_into_numpy_array(path):
+    """Load an image from file into a numpy array."""
+    image = Image.open(path)
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    return np.array(image)
+
+def show_mask(mask):
+    color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    return mask_image
+
+
+for idx, image_path in enumerate(IMAGE_PATHS):
+    print('Running inference for {}... '.format(image_path))
+
+    start = time.time()
+    image_np = load_image_into_numpy_array(image_path)
+
+    input_tensor = tf.convert_to_tensor(image_np)
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    detections = detect_fn(input_tensor)
+
+    image_np_with_detections = image_np.copy()
+
+    detection_boxes = tf.squeeze(detections['detection_boxes'], [0])
+    detection_masks = tf.squeeze(detections['detection_masks'], [0])
+
+    real_num_detection = tf.cast(detections['num_detections'][0], tf.int32)
+    detection_boxes = tf.slice(detection_boxes, [0, 0],
+                               [real_num_detection, -1])
+    detection_masks = tf.slice(detection_masks, [0, 0, 0],
+                               [real_num_detection, -1, -1])
+    detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+        detection_masks, detection_boxes, image_np.shape[0], image_np.shape[1])
+    detection_masks_reframed = tf.cast(
+        tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+    
+    detections['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
+
+    detections['num_detections'] = int(detections['num_detections'][0])
+    detections['detection_classes'] = detections['detection_classes'][0].numpy().astype(np.uint8)
+    detections['detection_boxes'] = detections['detection_boxes'][0].numpy()
+    detections['detection_scores'] = detections['detection_scores'][0].numpy()
+    detections['detection_masks'] = detections['detection_masks'][0].numpy()
+
+    image = Image.fromarray(np.uint8(image_np_with_detections))
+    image_np = np.array(image)
+    image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+
+    width, height = image.size
+    (ymin, xmin, ymax, xmax) = detections['detection_boxes'][0]
+    x_min, x_max, y_min, y_max = xmin * width, xmax * width, ymin * height, ymax * height # Boundig Box Coordinate
+    
+    center_x = (x_min + x_max) / 2
+    center_y = (y_min + y_max) / 2
+    point = np.array([[center_x, center_y]])
+    label = np.array([1])
+    
+    print(f"Bounding Box Coordinate: ({x_min}, {y_min}, {x_max}, {y_max})")
+
+    predictor.set_image(image_np)
+    masks, scores, logits = predictor.predict(
+        point_coords = point,
+        point_labels = label,
+        multimask_output = False
+    )
+
+    end = time.time()
+
+    mask_image = show_mask(masks)
+    # print(mask_image.shape)
+    # print(image_np.shape)
+    # alpha = 0.5
+    # beta = 1 - alpha
+    # img = cv2.addWeighted(image_np, alpha, mask_image, beta, 0)
+    output_path = f'images/test_annotated/{idx}.png'
+
+    cv2.imwrite(output_path, mask_image)  
+
+
+    cpu_usage, gpu_usage= log_hardware_usage()
+    print(cpu_usage)
+    print(gpu_usage)
+    print(f"Took {end - start} secs to annotate")
+    print('-' * 50, end='\n\n')
+
+print('Done!')
